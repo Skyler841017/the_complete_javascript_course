e63172f5ffdc8478ad3fc4a2aee27b4400f6de27
Jimmy <z10173425@gmail.com>
2019-04-16 22:06:13 +0800

    Section 5

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -1,49 +1,61 @@
-
-
 //Function constructor
-/*
-const  john = {
-   name: 'John',
-   yearOfBirth: 1990,
-   job: 'teacher'
- };
 
- const Person = function(name, yearOfBirth,job) {
-   this.name = name;
-   this.yearOfBirth = yearOfBirth;
-   this.job = job;
- }
- Person.prototype.calculateAge = 
- function (yearOfNow) {
-   console.log(yearOfNow - this.yearOfBirth);
- }
+// const Person = function(name, yearOfBirth, job) {
+//   this.name = name;
+//   this.yearOfBirth = yearOfBirth;
+//   this.job = job;
+// }
 
- const jimmy = new Person('Jimmy',1995,'programer');
- console.log(jimmy);
- jimmy.calculateAge(2019);
- */
+// const feather = new Person('Feather', 1995, 'teacher');
 
+// Person.prototype.calculateAge = function() {
+//   console.log(2019 - this.yearOfBirth);
+// }
 
- //Object.create
+// Person.prototype.lastName = 'Hung';
 
+// feather.calculateAge();
+// console.log(feather.lastName);
+// console.log(Person);
+// console.log(Person.prototype);
+// console.log(feather);
 
-
- //Primitives vs objects
+// // Person的prototype等於feather的繼承__proto__
+// console.log(Person.prototype === feather.__proto__);
 
 
 
- //First Class Functions
- const years = [1990, 1965, 1937, 2005, 1998];
+//Object.create
+const personProto = {
+  calculateAge: function() {
+    console.log(2019 - this.yearOfBirth);
+  }
+}
 
- function arrayCalc(arr, fn) {
-   const arrRes = [];
-   for (let i = 0; i < arr.length; i++) {
-     //.push方法可向數組的尾末添加一個或多個元素，並返回新的長度，
-     arrRes.push(fn(arr[i])); 
-    }
-    return arrRes;
- }
- 
+const john = Object.create(personProto);
+john.name = 'John';
+john.yearOfBirth = 1990;
+john.job = 'teacher';
+
+const jane = Object.create(personProto, {
+  name: { value: 'Jane' },
+  yearOfBirth: { value: 1886 },
+  job: { value: 'model' }
+})
+console.log(jane);
+
+//Primitives vs objects
 
 
-   
+
+//First Class Functions
+const years = [1990, 1965, 1937, 2005, 1998];
+
+function arrayCalc(arr, fn) {
+  const arrRes = [];
+  for (let i = 0; i < arr.length; i++) {
+    //.push方法可向數組的尾末添加一個或多個元素，並返回新的長度，
+    arrRes.push(fn(arr[i]));
+  }
+  return arrRes;
+}
